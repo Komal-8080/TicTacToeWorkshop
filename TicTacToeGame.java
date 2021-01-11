1968e4406e97fcbb1ed58c09ec5b5eaa8f1eeafd
Komal-8080 <shindeg163@gmail.com>
2021-01-11 09:52:53 +0530

    Added: Method to allow player to select X or O added and determining letter to player

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,10 +1,13 @@
 package tictactoeworkshop;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
 	char[] board;
+	static char player = ' ';
 
-	// uc1-creating game board
+	// UC1-creating game board
 	public static char[] gameBoard() {
 		char[] board = new char[10];
 		for (int i = 1; i < 10; i++) {
@@ -13,11 +16,31 @@ public class TicTacToeGame {
 		return board;
 	}
 
+	// UC2-Method to allow player to choose "x" or "o"
+	public static char selectXor0() {	
+		int loop = 0;		
+		while(loop == 0) {
+			System.out.println("Enter X or O");
+			Scanner read = new Scanner(System.in);
+			player = read.next().toUpperCase().charAt(0);
+				if(player == 'X' || player == 'O') {
+					return player;
+					}
+				else {
+					System.out.println ("Invalid Input");
+					continue;
+				}
+			
+			}
+		return player;
+		}
+
 	// Main method
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to TicTacToe");
 		gameBoard();
+		selectXor0();
 
 	}
 
