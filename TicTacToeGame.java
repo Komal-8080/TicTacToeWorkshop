0d40d75aa25431924e43127e111ba0a7918ae913
Komal-8080 <shindeg163@gmail.com>
2021-01-11 09:30:00 +0530

    Added: Creating a TicTacToe Board of size 10 and assiging empty space to each element

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -4,7 +4,7 @@ public class TicTacToeGame {
 
 	char[] board;
 
-	// uc1
+	// uc1-creating game board
 	public static char[] gameBoard() {
 		char[] board = new char[10];
 		for (int i = 1; i < 10; i++) {
@@ -13,6 +13,7 @@ public class TicTacToeGame {
 		return board;
 	}
 
+	// Main method
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to TicTacToe");
